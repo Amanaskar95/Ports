c0d01ae15cb6b9644f156424e5ee9f788085b27b
Aman Kumar <144769079+Amanaskar95@users.noreply.github.com>
2024-09-13 04:45:29 +0530

    first commit

---
+++ js/form.js
@@ -0,0 +1,4 @@
+// Clear the form fields when the page loads
+window.onload = function() {
+    document.getElementById('contactForm').reset();
+};
---
+++ js/script.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".typing", {
+    strings: ["Web Designer!", "𝑾eb 𝑫eveloper!", "Mern Stack Developer!", "Mentor!"],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop: true
+})
+
+ 
