42e66d96c070c9e078f10a884f74c3a5a351853d
Aman Kumar <144769079+Amanaskar95@users.noreply.github.com>
2024-12-02 12:18:20 +0530

    preloader

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
+
+// Function to hide the preloader and show the main content
+const loader = () => {
+    document.querySelector(".preloader").style.display = "none";
+    document.querySelector(".everything").style.display = "block";
+  };
+  
+  // Function to initialize the preloader with a delay
+  const preloader = () => {
+    setTimeout(loader, Math.floor(Math.random() * 3000) + 1000);
+  };
+  
 var typed = new Typed(".typing", {
     strings: ["Web Designer!", "𝑾eb 𝑫eveloper!", "Mern Stack Developer!", "Mentor!"],
     typeSpeed: 100,
